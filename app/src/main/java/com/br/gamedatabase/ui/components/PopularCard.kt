750db3f239207cca7ebcd19d3a6c7372598474e3
usuario-27554 <will.dutra26@gmail.com>
2025-11-20 15:56:47 -0300

    feat: criação do PopularCard

---
+++ app/src/main/java/com/br/gamedatabase/ui/components/PopularCard.kt
@@ -0,0 +1,136 @@
+package com.br.gamedatabase.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.br.gamedatabase.R
+import com.br.gamedatabase.ui.theme.GameDatabaseTheme
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun PopularCard(
+    modifier: Modifier = Modifier,
+    thumbUrl: String,
+    name: String,
+    category: String,
+    downloads: Int,
+    rating: Float,
+    reviews: Int
+) {
+    Card(
+        modifier = Modifier.height(118.dp)
+            .width(374.dp),
+        shape = RoundedCornerShape(25.dp),
+        colors = CardDefaults.cardColors(
+            contentColor = Color.White,
+            containerColor = Color.White,
+            disabledContainerColor = Color.White,
+            disabledContentColor = Color.White
+        )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Row() {
+
+                GlideImage(
+                    modifier = Modifier.height(90.dp)
+                        .width(160.dp),
+                    model = thumbUrl,
+                    contentDescription = "image",
+                    contentScale = ContentScale.Fit,
+                    requestBuilderTransform = {
+                        it.placeholder(R.drawable.ic_no_image_gallery)
+                    }
+                )
+
+                Column() {
+
+                    Text(
+                        text = name,
+                        fontFamily = poppisSemiBold,
+                        color = Color.Black
+                    )
+
+                    Text(
+                        text = category,
+                        fontFamily = poppis,
+                        fontSize = 10.sp,
+                        color = Color.Black
+                    )
+                    
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        Column {
+
+                            Text(
+                                text = "$downloads +",
+                                fontFamily = poppisSemiBold,
+                                fontSize = 10.sp,
+                                color = Color.Black
+                            )
+
+                            Text(
+                                text = "Downloads",
+                                fontFamily = poppis,
+                                fontSize = 8.sp,
+                                color = Color.Black
+                            )
+                        }
+
+                        Column {
+                            Text(
+                                text = rating.toString(),
+                                fontFamily = poppisSemiBold,
+                                fontSize = 10.sp,
+                                color = Color.Black
+                            )
+
+                            Text(
+                                text ="$reviews Reviews",
+                                fontFamily = poppis,
+                                fontSize = 8.sp,
+                                color = Color.Black)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF00FF00)
+@Composable
+private fun PopularCardPreview() {
+    GameDatabaseTheme {
+        PopularCard(
+            thumbUrl = "",
+            name = "Minecraft",
+            category = "Adventure, Online",
+            downloads = 100000,
+            rating = 5.0F,
+            reviews = 40000
+        )
+    }
+}
